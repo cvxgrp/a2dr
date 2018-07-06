Fix a sign flip error

cvxconsensus/consensus.py
@@ -184,7 +184,7 @@ def prox_step(prob, rho_init, scaled = False):
 		vmap[xid] = {"x": xvar, "xbar": Parameter(size, value = np.zeros(size)),
 					 "y": Parameter(size, value = np.zeros(size))}
 		dual = vmap[xid]["y"] if scaled else vmap[xid]["y"]/rho
-		f += (rho/2.0)*sum_squares(xvar - vmap[xid]["xbar"] - dual)
+		f += (rho/2.0)*sum_squares(xvar - vmap[xid]["xbar"] + dual)
 	
 	prox = Problem(Minimize(f), prob.constraints)
 	return prox, vmap, rho

cvxconsensus/tests/base_test.py
@@ -23,7 +23,7 @@ import numpy as np
 
 class BaseTest(TestCase):
     # AssertAlmostEqual for lists.
-    def assertItemsAlmostEqual(self, a, b, places=5):
+    def assertItemsAlmostEqual(self, a, b, places=4):
         if np.isscalar(a):
             a = [a]
         else:
@@ -36,7 +36,7 @@ class BaseTest(TestCase):
             self.assertAlmostEqual(a[i], b[i], places)
 
     # Overriden method to assume lower accuracy.
-    def assertAlmostEqual(self, a, b, places=5):
+    def assertAlmostEqual(self, a, b, places=4):
         super(BaseTest, self).assertAlmostEqual(a, b, places=places)
 
     def mat_to_list(self, mat):

cvxconsensus/tests/test_basic.py
@@ -51,7 +51,7 @@ class TestBasic(BaseTest):
 	def setUp(self):
 		np.random.seed(1)
 		self.MAX_ITER = 100
-		self.spectral = False
+		self.spectral = True
 	
 	def test_basic(self):
 		m = 100
@@ -87,7 +87,7 @@ class TestBasic(BaseTest):
 		N = len(probs.variables())
 		self.assertAlmostEqual(obj_admm, obj_comb)
 		for i in range(N):
-			self.assertItemsAlmostEqual(x_admm[i], x_comb[i], places = 4)
+			self.assertItemsAlmostEqual(x_admm[i], x_comb[i])
 
 	def test_ols(self):
 		N = 2
@@ -124,9 +124,9 @@ class TestBasic(BaseTest):
 		# Compare results.
 		# compare_results(probs, obj_admm, obj_comb, x_admm, x_comb)
 		N = len(probs.variables())
-		self.assertAlmostEqual(obj_admm, obj_comb, places = 1)
+		self.assertAlmostEqual(obj_admm, obj_comb)
 		for i in range(N):
-			self.assertItemsAlmostEqual(x_admm[i], x_comb[i], places = 2)
+			self.assertItemsAlmostEqual(x_admm[i], x_comb[i], places = 3)
 
 	def test_lasso(self):
 		""" FAILING: Objective blows up in consensus optimization."""
@@ -160,7 +160,7 @@ class TestBasic(BaseTest):
 		x_comb = [x.value for x in probs.variables()]
 		
 		# Compare results.
-		compare_results(probs, obj_admm, obj_comb, x_admm, x_comb)
+		# compare_results(probs, obj_admm, obj_comb, x_admm, x_comb)
 		N = len(probs.variables())
 		self.assertAlmostEqual(obj_admm, obj_comb)
 		for i in range(N):