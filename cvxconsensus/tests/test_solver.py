584f7b63839be159d128aeb9d89c47b1dec10aca
Anqi Fu <anqi.fu.1987@gmail.com>
2019-05-07 19:21:26 -0700

    WIP Adding a single commodity flow example with a small random network

--- cvxconsensus/tests/test_solver.py
+++ cvxconsensus/tests/test_solver.py
@@ -23,6 +23,7 @@ import numpy.linalg as LA
 import matplotlib.pyplot as plt
 from cvxpy import *
 from scipy.optimize import nnls
+from networkx import binomial_graph
 from cvxconsensus import a2dr
 from cvxconsensus.tests.base_test import BaseTest
 
@@ -38,8 +39,10 @@ def prox_neg_log_det(v, rho):
     n = int(np.sqrt(v.shape[0]))
     A = np.reshape(v, (n,n), order='C')
     A_symm = (A + A.T) / 2.0
-    if not (np.allclose(A, A_symm) and np.all(LA.eigvals(A_symm) > 0)):
-        raise Exception("Proximal operator for negative log-determinant only operates on symmetric positive definite matrices.")
+    # if not (np.allclose(A, A_symm) and np.all(LA.eigvals(A_symm) > 0)):
+    #    raise Exception("Proximal operator for negative log-determinant only operates on symmetric positive definite matrices.")
+    if not np.allclose(A, A_symm):
+        raise Exception("Proximal operator for negative log-determinant only operates on symmetric matrices.")
     U, s, Vt = LA.svd(A_symm, full_matrices=False)
     s_new = (s + np.sqrt(s ** 2 + 4.0/rho)) / 2
     A_new = U.dot(np.diag(s_new)).dot(Vt)
@@ -259,6 +262,102 @@ class TestSolver(BaseTest):
         plt.legend()
         plt.show()
 
+    def test_single_commodity_flow(self):
+        # Problem data.
+        m = 25  # Number of sources.
+        n = 300   # Number of flows.
+        A = np.random.randn(m,n)   # TODO: Make a proper incidence matrix.
+
+        # Flow cost = \sum_j \psi*(x_j - x0)^2 for 0 <= x_j <= x_max.
+        psi = 0.5
+        x0 = 0.5
+        x_max = 1
+
+        # Source costs.
+        # Generator cost = \sum_i \phi*s_i^2 for 0 <= s_i <= s_max.
+        m_free = 10  # Number of generators free to set.
+        m_off = 5    # Number of generators that are off (s_i = 0).
+        m_pin = 5    # Number of generators that are pinned to max (s_i = s_max).
+        m_gen = m_off + m_pin + m_free
+        phi = 0.5
+        s_max = 1
+
+        # Load cost = I(s_i = load_i) where load_i < 0.
+        m_load = m - m_gen   # Number of loads.
+        load = np.full(m_load, -1)
+
+        # Solve with CVXPY
+        x = Variable(n)
+        s_free = Variable(m_free)
+        s_off = Variable(m_off)
+        s_pin = Variable(m_pin)
+        s_load = Variable(m_load)
+        s = vec(vstack([reshape(s_free, (m_free,1)), reshape(s_off, (m_off,1)), \
+                        reshape(s_pin, (m_pin,1)), reshape(s_load, (m_load,1))]))
+        obj = phi*sum_squares(s_free) + psi*sum_squares(x - x0)
+        constr = [A*x + s == 0, sum(s) == 0, x >= 0, x <= x_max, s_free >= 0, s_free <= s_max,
+                  s_off == 0, s_pin == s_max, s_load == load]
+        prob = Problem(Minimize(obj), constr)
+        prob.solve()
+        cvxpy_obj = prob.value
+        cvxpy_x = x.value
+        cvxpy_s = s.value
+        print("CVXPY Objective:", cvxpy_obj)
+
+        # Initialize problem.
+        p_list = [lambda x,rho: (2*psi*x0 + rho*x)/(2*psi + rho),
+                  lambda s,rho: np.concatenate([rho*s[:m_free]/(2*phi + rho), s[m_free:]]),
+                  lambda x,rho: np.minimum(np.maximum(x,0), x_max),
+                  lambda s,rho: np.concatenate([np.minimum(np.maximum(s[:m_free],0), s_max), s[m_free:]])]
+        v_init = 2*[np.zeros(n), np.zeros(m)]
+        D = np.vstack([np.hstack([A, np.eye(m), np.zeros((m,m+n))]),
+                       np.hstack([np.zeros((1,n)), np.ones((1,m)), np.zeros((1,m+n))]),
+                       np.hstack([np.zeros((m_off,n + m_free)), np.eye(m_off), np.zeros((m_off, m_pin + m_load + m+n))]),
+                       np.hstack([np.zeros((m_pin,n + m_free + m_off)), np.eye(m_pin), np.zeros((m_pin, m_load + m+n))]),
+                       np.hstack([np.zeros((m_load,n + m_free + m_off + m_pin)), np.eye(m_load), np.zeros((m_load, m+n))]),
+                       np.hstack([np.eye(n), np.zeros((n,m)), -np.eye(n), np.zeros((n,m))]),
+                       np.hstack([np.zeros((m,n)), np.eye(m), np.zeros((m,n)), -np.eye(m)])
+                      ])
+        A_list = [D[:,:n], D[:,n:(n+m)], D[:,(n+m):(2*n+m)], D[:,(2*n+m):(2*n+2*m)]]
+        b = np.concatenate([np.zeros(m+1), np.zeros(m_off), np.full(m_pin, s_max), load, np.zeros(m+n)])
+
+        # Solve with DRS.
+        drs_result = a2dr(p_list, v_init, A_list, b, max_iter=self.MAX_ITER, eps_abs=self.eps_abs,
+                          eps_stop=self.eps_stop, anderson=False)
+        drs_x = drs_result["x_vals"][0]
+        drs_s = drs_result["x_vals"][1]
+        drs_obj = phi*np.sum(drs_s[:m_free]**2) + psi*np.sum((drs_x - x0)**2)
+        print("DRS Objective:", drs_obj)
+        self.assertAlmostEqual(cvxpy_obj, drs_obj)
+        self.assertItemsAlmostEqual(cvxpy_x, drs_x)
+        self.assertItemsAlmostEqual(cvxpy_s, drs_s)
+        self.plot_residuals(drs_result["primal"], drs_result["dual"], \
+                            normalize=True, title="DRS Residuals", semilogy=True)
+
+        # Solve with A2DR.
+        a2dr_result = a2dr(p_list, v_init, A_list, b, max_iter=self.MAX_ITER, eps_abs=self.eps_abs,
+                           eps_stop=self.eps_stop, anderson=True)
+        a2dr_x = a2dr_result["x_vals"][0]
+        a2dr_s = a2dr_result["x_vals"][1]
+        a2dr_obj = phi*np.sum(a2dr_s[:m_free] ** 2) + psi*np.sum((a2dr_x - x0) ** 2)
+        print("A2DR Objective:", a2dr_obj)
+        self.assertAlmostEqual(cvxpy_obj, a2dr_obj)
+        self.assertItemsAlmostEqual(cvxpy_x, a2dr_x)
+        self.assertItemsAlmostEqual(cvxpy_s, a2dr_s)
+        self.plot_residuals(a2dr_result["primal"], a2dr_result["dual"], \
+                            normalize=True, title="A2DR Residuals", semilogy=True)
+
+        # Compare residuals
+        plt.semilogy(range(drs_result["num_iters"]), drs_result["primal"], color="blue", linestyle="--",
+                     label="Primal (DRS)")
+        plt.semilogy(range(a2dr_result["num_iters"]), a2dr_result["primal"], color="blue", label="Primal (A2DR)")
+        plt.semilogy(range(drs_result["num_iters"]), drs_result["dual"], color="darkorange", linestyle="--",
+                     label="Dual (DRS)")
+        plt.semilogy(range(a2dr_result["num_iters"]), a2dr_result["dual"], color="darkorange", label="Dual (A2DR) ")
+        plt.title("Residuals")
+        plt.legend()
+        plt.show()
+
     def test_optimal_control(self):
         m = 2
         n = 5
@@ -287,9 +386,9 @@ class TestSolver(BaseTest):
 
         # Helper function for extracting (x_0,...,x_T) and (u_0,...,u_T) from z = (x_0, u_0, ..., x_T, u_T).
         def extract_xu(z):
-            z_mat = np.reshape(z, (m + n, T + 1), order='F')
-            x = z_mat[:n, :].ravel(order='F')
-            u = z_mat[n:, :].ravel(order='F')
+            z_mat = np.reshape(z, (m+n, T+1), order='F')
+            x = z_mat[:n,:].ravel(order='F')
+            u = z_mat[n:,:].ravel(order='F')
             return x, u
 
         # Calculate objective directly from z = (x_0, u_0, ..., x_T, u_T).
@@ -298,7 +397,16 @@ class TestSolver(BaseTest):
             u_inf = np.max(np.abs(u))
             return z.T.dot(QR_diag).dot(z) if u_inf <= u_bnd else np.inf
 
-        # Solve with CVXPY for comparison.
+        # Define proximal operators.
+        def prox_norm_inf_wrapper(z, rho):
+            z_mat = np.reshape(z, (m + n, T + 1), order='F')
+            u = z_mat[n:, :].ravel(order='F')
+            u_prox = prox_norm_inf(u_bnd)(u, rho)
+            u_mat = np.reshape(u_prox, (m, T + 1), order='F')
+            z_mat[n:, :] = u_mat
+            return z_mat.ravel(order='F')
+
+        # Solve with CVXPY.
         x = Variable((T+1,n))
         u = Variable((T+1,m))
         obj = sum([quad_form(x[t,:], Q) + quad_form(u[t,:], R) for t in range(T+1)])
@@ -318,17 +426,8 @@ class TestSolver(BaseTest):
         D = np.vstack(D_rows)
         e_vec = np.concatenate([x_init] + T*[c])
 
-        # Define proximal operators.
-        def prox_norm_inf_wrapper(z, rho):
-            z_mat = np.reshape(z, (m+n, T+1), order='F')
-            u = z_mat[n:,:].ravel(order='F')
-            u_prox = prox_norm_inf(u_bnd)(u, rho)
-            u_mat = np.reshape(u_prox, (m,T+1), order='F')
-            z_mat[n:,:] = u_mat
-            return z_mat.ravel(order='F')
-        p_list = [prox_quad_form(QR_diag), prox_norm_inf_wrapper]
-
         # Initialize problem.
+        p_list = [prox_quad_form(QR_diag), prox_norm_inf_wrapper]
         v_init = 2*[np.random.randn(K)]
         A_list = [np.vstack([D, np.eye(K)]), np.vstack([np.zeros((D.shape[0],K)), -np.eye(K)])]
         b = np.concatenate([e_vec, np.zeros(K)])
