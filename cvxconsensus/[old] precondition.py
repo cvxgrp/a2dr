1ef4f56dc0267ac28c43e3a92d87e462343dfb1c
junziz <junziz@stanford.edu>
2019-08-16 18:18:08 -0700

    more efficient precondition.py WIP

--- cvxconsensus/[old] precondition.py
+++ cvxconsensus/[old] precondition.py
@@ -1,18 +1,18 @@
 import numpy as np
-from scipy import sparse
 from scipy.sparse import block_diag, issparse, csr_matrix, diags
-from scipy.sparse.linalg import norm
 import scipy.linalg as sLA
 from scipy.stats.mstats import gmean
 
 def precondition(p_list, A_list, b, tol = 1e-3, max_iter = 1000):
     n_list = [A.shape[1] for A in A_list]
-    A = csr_matrix(sparse.hstack(A_list)) # enforce csr format for better efficiency
+    A_list_dense = [(A_list[i].todense() if issparse(A_list[i]) else A_list[i]) for i in range(len(A_list))]
+    A = np.array(np.hstack(A_list_dense))
     d, e, A_hat, k = mat_equil(A, n_list, tol, max_iter)
 
     split_idx = np.cumsum(n_list)
-    A_eq_list = np.split(A_hat.todense(), split_idx, axis=1)[:-1]
+    A_eq_list = np.split(A_hat, split_idx, axis=1)[:-1]
     A_eq_list = [csr_matrix(A_eq_list[i]) for i in range(len(A_eq_list))]
+    ### the original definition of p_eq_list was wrong: prox and ei are modified in place in the new lambda functions
     def proto(i, p_list, e):
         return lambda v, t: p_list[i](e[i]*v, t*e[i]**2)/e[i]
     p_eq_list = list(map(lambda i: proto(i,p_list,e), range(len(p_list))))
@@ -46,9 +46,9 @@ def mat_equil(A, n_list, tol, max_iter):
     
     # Form the size m-by-N matrix A_block, whose (i,j)-th entry is \sum_{k=n_1+...+n_{j-1}}}^{n_1+...+n_j} A_{ik}^2
     gamma = (m + N)/m/N * np.sqrt(np.finfo(float).eps)
-    A2 = A.power(2) 
+    A2 = A ** 2 
     ave_list = [np.ones([n_i,1]) for n_i in n_list]
-    A_block = A2.dot(block_diag(ave_list))
+    A_block = A2.dot(sLA.block_diag(*ave_list))
     A_block_T = A_block.transpose()
     print('Block matrix shape = {}'.format(A_block.shape))
     print('gamma={}'.format(gamma))
@@ -67,15 +67,15 @@ def mat_equil(A, n_list, tol, max_iter):
     
     d = np.sqrt(d)
     e = np.sqrt(e)
-    I_list = [sparse.eye(n_list[i]) * e[i] for i in range(N)]
-    E = csr_matrix(block_diag(I_list))
-    D = csr_matrix(diags(d))
+    I_list = [np.eye(n_list[i]) * e[i] for i in range(N)]
+    E = block_diag(I_list)
+    D = diags(d)
     print('generate D, E')
-    B = D.dot(csr_matrix(A).dot(E))
+    B = D.dot(csr_matrix(A).dot(E)).todense()
     print('compute scaled matrix')
     
     # Rescale to have \|DAE\|_2 close to 1
-    scale = norm(B, 'fro') / np.sqrt(np.min([m,N]))
+    scale = np.linalg.norm(B, 'fro') / np.sqrt(np.min([m,N]))
     d_mean = gmean(d)
     e_mean = gmean(e)
     q = np.log(d_mean / e_mean * scale) / 2 / np.log(scale)
