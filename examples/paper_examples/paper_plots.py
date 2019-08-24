correct bugs for large scale sparse inverse covariance estimation, improve performance of communication

examples/paper_examples/paper_plots.py
@@ -41,7 +41,7 @@ class TestPaper(BaseTest):
         np.random.seed(1)
         self.eps_rel = 1e-8   # specify these in all examples?
         self.eps_abs = 1e-6
-        self.MAX_ITER = 1000
+        self.MAX_ITER = 6000
 
     def test_nnls(self, figname):
         # minimize ||y - X\beta||_2^2 subject to \beta >= 0.
@@ -118,16 +118,25 @@ class TestPaper(BaseTest):
 
         S_true = sparse.csc_matrix(make_sparse_spd_matrix(n, ratio))
         R = sparse.linalg.inv(S_true).todense()
-        q_sample = sp.linalg.sqrtm(R).dot(np.random.randn(n,m))
+        q_sample = np.real(sp.linalg.sqrtm(R)).dot(np.random.randn(n,m)) # make sure it's real matrices
         Q = np.cov(q_sample)
+        print('Q is positive definite? {}'.format(bool(LA.slogdet(Q)[0])))
         mask = np.ones(Q.shape, dtype=bool)
         np.fill_diagonal(mask, 0)
         alpha_max = np.max(np.abs(Q)[mask])
         alpha = alpha_ratio*alpha_max #0.001 for n=100, 0.01 for n=50
+        
+#         ## Solve with CVXPY.
+#         S = Variable((n,n), PSD=True)
+#         obj = -log_det(S) + trace(S*Q) + alpha*norm1(S)
+#         prob = Problem(Minimize(obj))
+#         prob.solve(eps=self.eps_abs,verbose=True)
+#         cvxpy_obj = prob.value
+#         cvxpy_S = S.value
 
         # Convert problem to standard form.
-        # f_1(S) = -log(det(S)) on symmetric PSD matrices, f_2(S) = trace(S*Q), f_3(S) = \alpha*||S||_1.
-        # A_1 = [I; 0], A_2 = [-I; I], A_3 = [0; -I], b = 0.
+        # f_1(S) = -log(det(S)) + trace(S*Q) on symmetric PSD matrices, f_2(S) = \alpha*||S||_1.
+        # A_1 = I, A_2 = -I, b = 0.
         prox_list = [lambda v, t: prox_neg_log_det_aff(v.reshape((n,n), order='C'), Q, t, order='C'),
                      prox_norm1(alpha)]
         A_list = [sparse.eye(n*n), -sparse.eye(n*n)]
@@ -144,6 +153,21 @@ class TestPaper(BaseTest):
         self.compare_total(drs_result, a2dr_result, figname)
         print('Finished A2DR.')
         print('recovered sparsity = {}'.format(np.sum(a2dr_S!=0)*1.0/a2dr_S.shape[0]**2))
+        
+        a2dr_obj = -LA.slogdet(a2dr_S)[1] + np.sum(np.diag(a2dr_S.dot(Q))) + alpha*np.sum(np.abs(a2dr_S))
+        a2dr_S_2 = a2dr_result["x_vals"][0].reshape((n,n), order='C')
+        a2dr_obj_2 = -LA.slogdet(a2dr_S_2)[1] + np.sum(np.diag(a2dr_S_2.dot(Q))) + alpha*np.sum(np.abs(a2dr_S_2))
+        print('recovered sparsity 2 = {}'.format(np.sum(a2dr_S_2!=0)*1.0/a2dr_S_2.shape[0]**2))
+        #print(cvxpy_obj, a2dr_obj, a2dr_obj_2)
+        print(a2dr_obj, a2dr_obj_2)
+        print(a2dr_result['primal'])
+        print(a2dr_result['dual'])
+        
+        
+#         a2dr_S_ave = (a2dr_result["x_vals"][0] + a2dr_result["x_vals"][1])/2
+#         print(prox_list[0](a2dr_S_ave, 24.9999999))
+#         print(prox_list[1](a2dr_S_ave, 24.9999999))
+#         print(np.linalg.norm(prox_list[0](a2dr_S_ave, 24.9999999) - prox_list[1](a2dr_S_ave, 24.9999999)))
 
     def test_l1_trend_filtering(self, figname):
         # minimize (1/2)||y - x||_2^2 + \alpha*||Dx||_1,