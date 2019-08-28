3add96cef536976df62de76505c015624ab08b46
junziz <junziz@stanford.edu>
2019-08-28 02:17:32 -0700

    fix unicode issue, pass installation and tests, examples refactorization TODO

--- a2dr/proximal/constraint.py
+++ a2dr/proximal/constraint.py
@@ -5,14 +5,14 @@ from a2dr.proximal.composition import prox_scale
 
 def prox_box_constr(v, t = 1, v_lo = -np.inf, v_hi = np.inf, *args, **kwargs):
 	"""Proximal operator of :math:`tf(ax-b) + c^Tx + d\|x\|_2^2`, where :math:`f` is the set indicator that
-	:math:`\underline x \leq x \leq \overline x`. The scalar t > 0, and the optional arguments are a = scale,
+	:math:`\\underline x \leq x \leq \overline x`. The scalar t > 0, and the optional arguments are a = scale,
 	b = offset, c = lin_term, and d = quad_term. We must have t > 0, a = non-zero, and d > 0. By default, t = 1,
 	a = 1, b = 0, c = 0, and d = 0.
 	"""
 	return prox_scale(prox_box_constr_base, v_lo, v_hi, *args, **kwargs)(v, t)
 
 def prox_box_constr_base(v, t, v_lo, v_hi):
-	"""Proximal operator of the set indicator that :math:`\underline x \leq x \leq \overline x`.
+	"""Proximal operator of the set indicator that :math:`\\underline x \leq x \leq \overline x`.
 	"""
 	if sparse.issparse(v):
 		max_elemwise = lambda x, y: x.maximum(y)
